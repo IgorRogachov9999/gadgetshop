124bb329043eafb70fc1949e69de5aaaeb268168
Igorrogachov9999 <igorrogachov9999@gmail.com>
2019-07-03 11:59:57 +0300

    Models and static added.

--- gadgetshop/gshopapp/models.py
+++ gadgetshop/gshopapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, models.CASCADE)
+    brand = models.ForeignKey(Brand, models.CASCADE)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.title
