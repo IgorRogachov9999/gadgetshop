4d97d08c2b532d385ffe993218fa19c9017fba2e
Igorrogachov9999 <igorrogachov9999@gmail.com>
2019-07-04 16:00:54 +0300

    AJAX added.

--- gadgetshop/gshopapp/urls.py
+++ gadgetshop/gshopapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from gshopapp.views import base_view, product_view, category_view, \
-                           cart_view, add_to_cart_view, remove_from_cart_view
+                           cart_view, add_to_cart_view, \
+                           remove_from_cart_view, change_item_qty
+                        
 
 urlpatterns = [
     path('', base_view, name='base'),
     path('product/<slug:product_slug>', product_view, name='product_detail'),
     path('category/<slug:category_slug>', category_view, name='category_detail'),
-    path('add_to_cart/<slug:product_slug>', add_to_cart_view, name='add_to_cart'),
-    path('remove_from_cart/<slug:product_slug>', remove_from_cart_view, name='remove_from_cart'),
+    path('add_to_cart/', add_to_cart_view, name='add_to_cart'),
+    path('remove_from_cart/', remove_from_cart_view, name='remove_from_cart'),
+    path('change_item_qty/', change_item_qty, name='change_item_qty'),
     path('cart/', cart_view, name='cart'),
 ]
--- gadgetshop/gshopapp/views.py
+++ gadgetshop/gshopapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from gshopapp.models import Category, Product, CartItem, Cart
 
 
-def base_view(request):
+def get_cart(request):
     try:
         cart_id = request.session['cart_id']
         cart = Cart.objects.get(id=cart_id)
@@ -13,6 +13,11 @@ def base_view(request):
         cart = Cart()
         cart.save()
         request.session['cart_id'] = cart.id
+    return cart
+
+
+def base_view(request):
+    cart = get_cart(request)
     products = Product.objects.all()
     categories = Category.objects.all()
     context = {
@@ -24,14 +29,7 @@ def base_view(request):
     
 
 def product_view(request, product_slug):
-    try:
-        cart_id = request.session['cart_id']
-        cart = Cart.objects.get(id=cart_id)
-        request.session['total'] = cart.items.count()
-    except:
-        cart = Cart()
-        cart.save()
-        request.session['cart_id'] = cart.id
+    cart = get_cart(request)
     product = Product.objects.get(slug=product_slug)
     categories = Category.objects.all()
     context = {
@@ -43,14 +41,7 @@ def product_view(request, product_slug):
 
 
 def category_view(request, category_slug):
-    try:
-        cart_id = request.session['cart_id']
-        cart = Cart.objects.get(id=cart_id)
-        request.session['total'] = cart.items.count()
-    except:
-        cart = Cart()
-        cart.save()
-        request.session['cart_id'] = cart.id
+    cart = get_cart(request)
     category = Category.objects.get(slug=category_slug)
     products_of_category = Product.objects.filter(category=category)
     categories = Category.objects.all()
@@ -64,14 +55,7 @@ def category_view(request, category_slug):
 
 
 def cart_view(request):
-    try:
-        cart_id = request.session['cart_id']
-        cart = Cart.objects.get(id=cart_id)
-        request.session['total'] = cart.items.count()
-    except:
-        cart = Cart()
-        cart.save()
-        request.session['cart_id'] = cart.id
+    cart = get_cart(request)
     categories = Category.objects.all()
     context = {
         'cart': cart,
@@ -80,29 +64,56 @@ def cart_view(request):
     return render(request, 'cart.html', context)
 
 
-def add_to_cart_view(request, product_slug):
-    try:
-        cart_id = request.session['cart_id']
-        cart = Cart.objects.get(id=cart_id)
-        request.session['total'] = cart.items.count()
-    except:
-        cart = Cart()
-        cart.save()
-        request.session['cart_id'] = cart.id
+def add_to_cart_view(request):
+    cart = get_cart(request)
+    product_slug = request.GET.get('product_slug')
     product = Product.objects.get(slug=product_slug)
     cart.add_to_cart(product)
-    return HttpResponseRedirect(reverse('cart'))
+    new_cart_total = 0.00
+    for item in cart.items.all():
+        new_cart_total += float(item.item_total)
+    cart.cart_total = new_cart_total
+    cart.save()
+    data = {
+        'cart_total': cart.items.count(),
+        'cart_total_price': new_cart_total
+    }
+    return JsonResponse(data)
 
 
-def remove_from_cart_view(request, product_slug):
-    try:
-        cart_id = request.session['cart_id']
-        cart = Cart.objects.get(id=cart_id)
-        request.session['total'] = cart.items.count()
-    except:
-        cart = Cart()
-        cart.save()
-        request.session['cart_id'] = cart.id
+def remove_from_cart_view(request):
+    cart = get_cart(request)
+    product_slug = request.GET.get('product_slug')
     product = Product.objects.get(slug=product_slug)
     cart.remove_from_cart(product)
-    return HttpResponseRedirect(reverse('cart'))
+    new_cart_total = 0.00
+    for item in cart.items.all():
+        new_cart_total += float(item.item_total)
+    cart.cart_total = new_cart_total
+    cart.save()
+    data = {
+        'cart_total': cart.items.count(),
+        'cart_total_price': new_cart_total
+    }
+    return JsonResponse(data)
+
+
+def change_item_qty(request):
+    cart = get_cart(request)
+    qty = int(request.GET.get('qty'))
+    item_id = int(request.GET.get('item_id'))
+    cart_item = CartItem.objects.get(id=item_id)
+    cart_item.qty = qty
+    cart_item.item_total = qty * float(cart_item.product.price)
+    cart_item.save()
+    new_cart_total = 0.00
+    for item in cart.items.all():
+        new_cart_total += float(item.item_total)
+    cart.cart_total = new_cart_total
+    cart.save()
+    data = {
+        'cart_total': cart.items.count(),
+        'item_total': cart_item.item_total,
+        'cart_total_price': new_cart_total
+    }
+    return JsonResponse(data)
